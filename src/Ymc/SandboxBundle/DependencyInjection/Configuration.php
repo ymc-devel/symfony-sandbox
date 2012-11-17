16d5f364252f3403d07cbfeae35e8092feacff6c
Frank Neff <fneff89@gmail.com>
2012-11-17 17:11:19 +0100

    Simple sandbox for SF2

--- src/Ymc/SandboxBundle/DependencyInjection/Configuration.php
+++ src/Ymc/SandboxBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ymc\SymfonyHandsOnBundle\DependencyInjection;
+namespace Ymc\SandboxBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ymc_symfony_hands_on');
+        $rootNode = $treeBuilder->root('ymc_sandbox');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
